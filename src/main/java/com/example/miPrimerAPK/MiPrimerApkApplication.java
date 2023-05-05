c3a294c6d1a745cee3f47fbcf8d8ffa5b0d369a8
Sebas-Quiroga <125420726+Sebas-Quiroga@users.noreply.github.com>
2023-05-04 20:48:05 -0500

    inyeccion del proyecto
    se agg el proyecto al repositorio

---
+++ src/main/java/com/example/miPrimerAPK/MiPrimerApkApplication.java
@@ -0,0 +1,13 @@
+package com.example.miPrimerAPK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerApkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerApkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miPrimerAPK/MiPrimerApkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miPrimerAPK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerApkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
